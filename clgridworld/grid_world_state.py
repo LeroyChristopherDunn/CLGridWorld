2bfdaacf8209e41e13a751363b36d2478996cb7c
leroy <leroy.c.dunn@gmail.com>
2019-03-13 17:44:06 +0200

    refactor: separated pit coords into two params and made both inclusive

--- clgridworld/grid_world_state.py
+++ clgridworld/grid_world_state.py
@@ -6,7 +6,8 @@ class GridWorldState:
     PLAYER_KEY = "player"
     KEY_DICT_KEY = "key"
     LOCK_KEY = "lock"
-    PIT_KEY = "pit"
+    PIT_START_KEY = "pit_start"
+    PIT_END_KEY = "pit_end"
     NW_BEACON_KEY = "nw_beacon"
     NE_BEACON_KEY = "ne_beacon"
     SW_BEACON_KEY = "sw_beacon"
@@ -14,20 +15,21 @@ class GridWorldState:
     HAS_KEY_DICT_KEY = "has_key"
 
     @staticmethod
-    def create(shape: tuple, player_coords: tuple, key_coords: tuple, lock_coords: tuple, pit_coords: tuple) -> dict:
+    def create(shape: tuple, player_coords: tuple, key_coords: tuple, lock_coords: tuple, pit_start_coords: tuple, pit_end_coords: tuple) -> dict:
 
-        GridWorldState._validate_coords_are_in_bounds(shape, player_coords, key_coords, lock_coords, pit_coords)
-        GridWorldState._validate_coords_dont_overlap(player_coords, key_coords, lock_coords, pit_coords)
+        GridWorldState._validate_coords_are_in_bounds(shape, player_coords, key_coords, lock_coords, pit_start_coords, pit_end_coords)
+        GridWorldState._validate_coords_dont_overlap(player_coords, key_coords, lock_coords, pit_start_coords, pit_end_coords)
 
         ne_beacon_coords, nw_beacon_coords, se_beacon_coords, sw_beacon_coords = \
-            GridWorldState._get_pit_beacon_coords(pit_coords)
+            GridWorldState._get_pit_beacon_coords(pit_start_coords, pit_end_coords)
 
         return {
             GridWorldState.GRID_SHAPE_KEY:      shape,
             GridWorldState.PLAYER_KEY:          player_coords,
             GridWorldState.KEY_DICT_KEY:        key_coords,
             GridWorldState.LOCK_KEY:            lock_coords,
-            GridWorldState.PIT_KEY:             pit_coords,
+            GridWorldState.PIT_START_KEY:       pit_start_coords,
+            GridWorldState.PIT_END_KEY:         pit_end_coords,
             GridWorldState.NW_BEACON_KEY:       nw_beacon_coords,
             GridWorldState.NE_BEACON_KEY:       ne_beacon_coords,
             GridWorldState.SW_BEACON_KEY:       sw_beacon_coords,
@@ -36,7 +38,7 @@ class GridWorldState:
         }
 
     @staticmethod
-    def _validate_coords_are_in_bounds(shape, player_coords, key_coords, lock_coords, pit_coords):
+    def _validate_coords_are_in_bounds(shape, player_coords, key_coords, lock_coords, pit_start_coords, pit_end_coords):
 
         if not GridWorldState._is_in_shape_bounds(player_coords, shape):
             raise ValueError("Player coords %s not in shape bounds %s" % (player_coords, shape))
@@ -47,12 +49,14 @@ class GridWorldState:
         if not GridWorldState._is_in_shape_bounds(lock_coords, shape):
             raise ValueError("lock coords %s not in shape bounds %s" % (lock_coords, shape))
 
-        for pit_coord in pit_coords:
-            if not GridWorldState._is_in_shape_bounds(pit_coord, shape):
-                raise ValueError("lock coords %s not in shape bounds %s" % (pit_coord, shape))
+        if not GridWorldState._is_in_shape_bounds(pit_start_coords, shape):
+            raise ValueError("lock coords %s not in shape bounds %s" % (pit_start_coords, shape))
+
+        if not GridWorldState._is_in_shape_bounds(pit_end_coords, shape):
+            raise ValueError("lock coords %s not in shape bounds %s" % (pit_end_coords, shape))
 
     @staticmethod
-    def _validate_coords_dont_overlap(player_coords, key_coords, lock_coords, pit_coords):
+    def _validate_coords_dont_overlap(player_coords, key_coords, lock_coords, pit_start_coords, pit_end_coords):
 
         if player_coords == key_coords:
             raise ValueError("player coords %s equal to key coords %s" % (player_coords, key_coords))
@@ -63,38 +67,35 @@ class GridWorldState:
         if key_coords == lock_coords:
             raise ValueError("key coords %s equal to lock coords %s" % (player_coords, key_coords))
 
-        pit_row_start_incl = pit_coords[0][0]
-        pit_col_start_incl = pit_coords[0][1]
-        pit_row_end_excl = pit_coords[1][0]
-        pit_col_end_excl = pit_coords[1][1]
+        pit_row_start = pit_start_coords[0]
+        pit_col_start = pit_start_coords[1]
+        pit_row_end = pit_end_coords[0]
+        pit_col_end = pit_end_coords[1]
 
-        if pit_row_start_incl <= player_coords[0] <= pit_row_end_excl \
-                and pit_col_start_incl <= player_coords[1] < pit_col_end_excl:
-            raise ValueError("player coords %s within pit coords %s" % (player_coords, pit_coords))
+        if pit_row_start <= player_coords[0] <= pit_row_end and pit_col_start <= player_coords[1] <= pit_col_end:
+            raise ValueError("player coords %s within pit start coords %s and pit end coords %s" % (player_coords, pit_start_coords, pit_end_coords))
 
-        if pit_row_start_incl <= lock_coords[0] <= pit_row_end_excl \
-                and pit_col_start_incl <= lock_coords[1] < pit_col_end_excl:
-            raise ValueError("lock coords %s within pit coords %s" % (lock_coords, pit_coords))
+        if pit_row_start <= lock_coords[0] <= pit_row_end and pit_col_start <= lock_coords[1] <= pit_col_end:
+            raise ValueError("lock coords %s within pit start coords %s and pit end coords %s" % (lock_coords, pit_start_coords, pit_end_coords))
 
-        if pit_row_start_incl <= key_coords[0] <= pit_row_end_excl \
-                and pit_col_start_incl <= key_coords[1] < pit_col_end_excl:
-            raise ValueError("key coords %s within pit coords %s" % (key_coords, pit_coords))
+        if pit_row_start <= key_coords[0] <= pit_row_end and pit_col_start <= key_coords[1] <= pit_col_end:
+            raise ValueError("key coords %s within pit start coords %s and pit end coords %s" % (key_coords, pit_start_coords, pit_end_coords))
 
     @staticmethod
     def _is_in_shape_bounds(point: tuple, shape: tuple) -> bool:
         return 0 <= point[0] < shape[0] and 0 <= point[1] < shape[1]
 
     @staticmethod
-    def _get_pit_beacon_coords(pit_coords):
+    def _get_pit_beacon_coords(pit_start_coords, pit_end_coords):
 
-        pit_row_start_incl = pit_coords[0][0]
-        pit_col_start_incl = pit_coords[0][1]
-        pit_row_end_excl = pit_coords[1][0]
-        pit_col_end_excl = pit_coords[1][1]
+        pit_row_start = pit_start_coords[0]
+        pit_col_start = pit_start_coords[1]
+        pit_row_end = pit_end_coords[0]
+        pit_col_end = pit_end_coords[1]
 
-        nw_beacon_coords = (pit_row_start_incl - 1, pit_col_start_incl - 1)
-        ne_beacon_coords = (pit_row_start_incl - 1, pit_col_end_excl)
-        sw_beacon_coords = (pit_row_end_excl, pit_col_start_incl - 1)
-        se_beacon_coords = (pit_row_end_excl, pit_col_end_excl)
+        nw_beacon_coords = (pit_row_start - 1, pit_col_start - 1)
+        ne_beacon_coords = (pit_row_start - 1, pit_col_end + 1)
+        sw_beacon_coords = (pit_row_end + 1, pit_col_start - 1)
+        se_beacon_coords = (pit_row_end + 1, pit_col_end + 1)
 
         return ne_beacon_coords, nw_beacon_coords, se_beacon_coords, sw_beacon_coords
--- tests/test_grid_world_state.py
+++ tests/test_grid_world_state.py
@@ -6,10 +6,10 @@ from clgridworld.grid_world_state import GridWorldState
 class TestGridWorldState(TestCase):
 
     @staticmethod
-    def create_state_with_spec(shape=(10, 10), player_coords=(1, 4), key_coords=(7, 5), lock_coords=(1, 1), pit_coords=((4, 2), (5, 8))):
+    def create_state_with_spec(shape=(10, 10), player_coords=(1, 4), key_coords=(7, 5), lock_coords=(1, 1), pit_start_coords=(4, 2), pit_end_coords=(4, 7)):
 
         #  defaults to target task spec in 'Autonomous Task Sequencing... Narvekar et al 2017'
-        return GridWorldState.create(shape, player_coords, key_coords, lock_coords, pit_coords)
+        return GridWorldState.create(shape, player_coords, key_coords, lock_coords, pit_start_coords, pit_end_coords)
 
     def test_given_valid_parameters_should_correctly_create_state(self):
 
@@ -18,20 +18,22 @@ class TestGridWorldState(TestCase):
         player_coords = (1, 4)
         key_coords = (7, 5)
         lock_coords = (1, 1)
-        pit_coords = ((4, 2), (5, 8))
+        pit_start_coords = (4, 2)
+        pit_end_coords = (4, 7)
 
         nw_beacon = (3, 1)
         ne_beacon = (3, 8)
         sw_beacon = (5, 1)
         se_beacon = (5, 8)
 
-        state = GridWorldState.create(shape, player_coords, key_coords, lock_coords, pit_coords)
+        state = GridWorldState.create(shape, player_coords, key_coords, lock_coords, pit_start_coords, pit_end_coords)
 
         self.assertEqual(shape, state[GridWorldState.GRID_SHAPE_KEY], "shape not equal")
         self.assertEqual(player_coords, state[GridWorldState.PLAYER_KEY], "player coords not equal")
         self.assertEqual(key_coords, state[GridWorldState.KEY_DICT_KEY], "key coords not equal")
         self.assertEqual(lock_coords, state[GridWorldState.LOCK_KEY], "lock coords not equal")
-        self.assertEqual(pit_coords, state[GridWorldState.PIT_KEY], "pit coords not equal")
+        self.assertEqual(pit_start_coords, state[GridWorldState.PIT_START_KEY], "pit start key not equal")
+        self.assertEqual(pit_end_coords, state[GridWorldState.PIT_END_KEY], "pit end key not equal")
         self.assertEqual(nw_beacon, state[GridWorldState.NW_BEACON_KEY], "nw beacon coords not equal")
         self.assertEqual(ne_beacon, state[GridWorldState.NE_BEACON_KEY], "ne beacon coords not equal")
         self.assertEqual(sw_beacon, state[GridWorldState.SW_BEACON_KEY], "sw beacon coords not equal")
@@ -55,8 +57,8 @@ class TestGridWorldState(TestCase):
 
     def test_given_pit_coords_out_of_bounds_should_throw_error(self):
 
-        self.assertRaises(ValueError, TestGridWorldState.create_state_with_spec, pit_coords=((-1, 2), (5, 8)), shape=(10, 10))
-        self.assertRaises(ValueError, TestGridWorldState.create_state_with_spec, pit_coords=((4, 2), (5, 10)), shape=(10, 10))
+        self.assertRaises(ValueError, TestGridWorldState.create_state_with_spec, pit_start_coords=(-1, 2), pit_end_coords=(4, 7), shape=(10, 10))
+        self.assertRaises(ValueError, TestGridWorldState.create_state_with_spec, pit_start_coords=(4, 2), pit_end_coords=(4, 10), shape=(10, 10))
 
     def test_given_player_coords_overlap_with_key_coords_should_throw_error(self):
 
@@ -72,14 +74,14 @@ class TestGridWorldState(TestCase):
 
     def test_given_player_coords_overlap_with_pit_coords_should_throw_error(self):
 
-        self.assertRaises(ValueError, TestGridWorldState.create_state_with_spec, player_coords=(4, 2), pit_coords=((4, 2), (5, 8)))
+        self.assertRaises(ValueError, TestGridWorldState.create_state_with_spec, player_coords=(4, 2), pit_start_coords=(4, 2), pit_end_coords=(4, 7))
 
     def test_given_lock_coords_overlap_with_pit_coords_should_throw_error(self):
 
-        self.assertRaises(ValueError, TestGridWorldState.create_state_with_spec, lock_coords=(5, 3), pit_coords=((4, 2), (6, 8)))
+        self.assertRaises(ValueError, TestGridWorldState.create_state_with_spec, lock_coords=(5, 3), pit_start_coords=(4, 2), pit_end_coords=(5, 7))
 
     def test_given_key_coords_overlap_with_pit_coords_should_throw_error(self):
 
-        self.assertRaises(ValueError, TestGridWorldState.create_state_with_spec, key_coords=(5, 7), pit_coords=((4, 2), (6, 8)))
+        self.assertRaises(ValueError, TestGridWorldState.create_state_with_spec, key_coords=(5, 7), pit_start_coords=(4, 2), pit_end_coords=(5, 7))
 
 
