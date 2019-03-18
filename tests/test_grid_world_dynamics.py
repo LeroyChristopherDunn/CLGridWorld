47eb3f61c98c27600eeee41a8ee34d540da3c9e3
leroy <leroy.c.dunn@gmail.com>
2019-03-18 19:44:10 +0200

    refactor: parameterized player moves into key tests

--- tests/test_grid_world_dynamics.py
+++ tests/test_grid_world_dynamics.py
@@ -105,49 +105,22 @@ class TestGridWorldDynamics(TestCase):
                 self.assertDictEqual(expected_state, new_state)
                 self.assertTrue(new_state.is_in_pit())
 
-    def test_when_player_moves_north_into_key_should_remain_in_same_state(self):
+    def test_when_player_moves_into_key_should_remain_in_same_state(self):
 
-        player_coords = (1, 0)
-        key_coords = (0, 0)
+        player_start_coords =   [(1, 0), (0, 0), (0, 0), (0, 1)]
+        key_start_coords =      [(0, 0), (0, 1), (1, 0), (0, 0)]
 
-        state = GridWorldStateBuilder.create_state_with_spec(player_coords=player_coords, key_coords=key_coords)
+        for i in range(len(self.actions)):
+            with self.subTest(action=self.action_names[i]):
 
-        new_state = GridWorldDynamics(state).step(GridWorldActions.NORTH)
+                player_coords = player_start_coords[i]
+                action = self.actions[i]
+                key_coords = key_start_coords[i]
 
-        self.assertEqual(state, new_state)
+                state = GridWorldStateBuilder.create_state_with_spec(player_coords=player_coords, key_coords=key_coords)
+                new_state = GridWorldDynamics(state).step(action)
 
-    def test_when_player_moves_east_into_key_should_remain_in_same_state(self):
-
-        player_coords = (0, 0)
-        key_coords = (0, 1)
-
-        state = GridWorldStateBuilder.create_state_with_spec(player_coords=player_coords, key_coords=key_coords)
-
-        new_state = GridWorldDynamics(state).step(GridWorldActions.EAST)
-
-        self.assertEqual(state, new_state)
-
-    def test_when_player_moves_south_into_key_should_remain_in_same_state(self):
-
-        player_coords = (0, 0)
-        key_coords = (1, 0)
-
-        state = GridWorldStateBuilder.create_state_with_spec(player_coords=player_coords, key_coords=key_coords)
-
-        new_state = GridWorldDynamics(state).step(GridWorldActions.SOUTH)
-
-        self.assertEqual(state, new_state)
-
-    def test_when_player_moves_west_into_key_should_remain_in_same_state(self):
-
-        player_coords = (0, 1)
-        key_coords = (0, 0)
-
-        state = GridWorldStateBuilder.create_state_with_spec(player_coords=player_coords, key_coords=key_coords)
-
-        new_state = GridWorldDynamics(state).step(GridWorldActions.WEST)
-
-        self.assertEqual(state, new_state)
+                self.assertDictEqual(state, new_state)
 
     def test_when_player_moves_north_into_lock_should_remain_in_same_state(self):
 
