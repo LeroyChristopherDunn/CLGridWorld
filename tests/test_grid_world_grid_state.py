4e9a4584a09bc9c0550ff14a0d4377940d13d28b
leroy <leroy.c.dunn@gmail.com>
2019-03-18 18:42:31 +0200

    refactor: removed redundant variable

--- tests/test_grid_world_grid_state.py
+++ tests/test_grid_world_grid_state.py
@@ -13,40 +13,40 @@ class TestGridWorldGridState(TestCase):
         shape = (20, 25)
 
         state = GridWorldStateBuilder.create_state_with_spec(shape=shape)
-        grid_state = GridWorldGridState(state)
+        grid = GridWorldGridState(state).grid
 
-        self.assertEqual(shape, grid_state.grid.shape)
+        self.assertEqual(shape, grid.shape)
 
     def test_player_coords(self):
 
         player_coords = (2, 3)
 
         state = GridWorldStateBuilder.create_state_with_spec(player_coords=player_coords)
-        grid_state = GridWorldGridState(state)
+        grid = GridWorldGridState(state).grid
 
-        self.assertEqual(GridWorldGridState.PLAYER, grid_state.grid[player_coords])
+        self.assertEqual(GridWorldGridState.PLAYER, grid[player_coords])
 
         # alternative notations
-        self.assertEqual(GridWorldGridState.PLAYER, grid_state.grid[player_coords[0], player_coords[1]])
-        self.assertEqual(GridWorldGridState.PLAYER, grid_state.grid[player_coords[0]][player_coords[1]])
+        self.assertEqual(GridWorldGridState.PLAYER, grid[player_coords[0], player_coords[1]])
+        self.assertEqual(GridWorldGridState.PLAYER, grid[player_coords[0]][player_coords[1]])
 
     def test_key_coords(self):
 
         key_coords = (5, 4)
 
         state = GridWorldStateBuilder.create_state_with_spec(key_coords=key_coords)
-        grid_state = GridWorldGridState(state)
+        grid = GridWorldGridState(state).grid
 
-        self.assertEqual(GridWorldGridState.KEY, grid_state.grid[key_coords])
+        self.assertEqual(GridWorldGridState.KEY, grid[key_coords])
 
     def test_lock_coords(self):
 
         lock_coords = (5, 4)
 
         state = GridWorldStateBuilder.create_state_with_spec(lock_coords=lock_coords)
-        grid_state = GridWorldGridState(state)
+        grid = GridWorldGridState(state).grid
 
-        self.assertEqual(GridWorldGridState.LOCK, grid_state.grid[lock_coords])
+        self.assertEqual(GridWorldGridState.LOCK, grid[lock_coords])
 
     def test_pit_coords(self):
 
@@ -54,9 +54,9 @@ class TestGridWorldGridState(TestCase):
         pit_end = (4, 7)
 
         state = GridWorldStateBuilder.create_state_with_spec(pit_start_coords=pit_start, pit_end_coords=pit_end)
-        grid_state = GridWorldGridState(state)
+        grid = GridWorldGridState(state).grid
 
-        pit_state = grid_state.grid[pit_start[0]:pit_end[0]+1, pit_start[1]:pit_end[1]+1]
+        pit_state = grid[pit_start[0]:pit_end[0]+1, pit_start[1]:pit_end[1]+1]
 
         self.assertTrue(np.all(pit_state == GridWorldGridState.PIT))
 
