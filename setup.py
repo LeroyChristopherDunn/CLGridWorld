e5fb9bbaf6a33bf03608e569bf23e30954c458be
leroy <leroy.c.dunn@gmail.com>
2019-03-23 19:14:52 +0200

    fix: fixed packages

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='gym_clgridworld',
     version='1.0.0',
-    packages=['clgridworld, example, tests'],
+    packages=['clgridworld', 'example', 'tests'],
     url='https://github.com/LeroyChristopherDunn/CurriculumLearningGridWorld',
     license='MIT',
     author='Leroy Christopher Dunn',
