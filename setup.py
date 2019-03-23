95b8f93391dc79c5f7ebd1ea7358293904e07c64
leroy <leroy.c.dunn@gmail.com>
2019-03-23 17:52:34 +0200

    feat: setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='gym_clgridworld',
+    version='1.0.0',
+    packages=['clgridworld, example, tests'],
+    url='https://github.com/LeroyChristopherDunn/CurriculumLearningGridWorld',
+    license='MIT',
+    author='Leroy Christopher Dunn',
+    author_email='social.leroy.c.dunn@gmail.com',
+    description='Curriculum Learning Environment for Reinforcement Learning Agents',
+    install_requires=['gym', 'numpy', 'matplotlib']
+)
